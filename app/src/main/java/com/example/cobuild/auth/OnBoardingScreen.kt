8db7f07b3eafa63030a0ec8f34326c601b982555
Ajay Patidar <ajaypatidar.it@gmail.com>
2025-11-14 17:49:21 +0530

    OnBoardingScreen.kt added

---
+++ app/src/main/java/com/example/cobuild/auth/OnBoardingScreen.kt
@@ -0,0 +1,25 @@
+package com.example.cobuild
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OnBoardingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Welcome to OnBoarding!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingScreenPreview() {
+    OnBoardingScreen()
+}
