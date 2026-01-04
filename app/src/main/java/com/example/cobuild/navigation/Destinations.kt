eb36d59a7d01ae9b122a7631c5baddab60e5fb6a
Ajay Patidar <ajaypatidar.it@gmail.com>
2026-01-04 20:04:15 +0530

    Destinations.kt added

---
+++ app/src/main/java/com/example/cobuild/navigation/Destinations.kt
@@ -0,0 +1,14 @@
+package com.example.cobuild.navigation
+
+object Destinations {
+    const val LOGIN = "login"
+    const val ONBOARDING = "onboarding"
+    const val HOME = "home"
+    const val ADD_PROJECT = "add_project"
+    const val PROFILE = "profile"
+
+    const val PROJECT_LIST = "project_list"
+
+    const val PROJECT_DETAIL = "project_detail"
+    const val PROJECT_DETAIL_ROUTE = "project_detail/{projectId}"
+}
